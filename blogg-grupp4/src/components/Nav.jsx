185dcb4ca11bd4834fb7170c529ced8a9affe4a1
anton <jappinen.anton@protonmail.com>
2025-08-28 10:29:31 +0200

    init 2

---
+++ blogg-grupp4/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+
+export function Navbar() {
+return (
+      <>
+        <nav>
+          <ul>
+            <li>anton</li>
+            <li>estelle</li>
+            <li>simon</li>
+          </ul>
+          </nav> 
+      </>
+)
+}
