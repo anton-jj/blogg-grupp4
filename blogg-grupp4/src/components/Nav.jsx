b87607a01ecfc84a16ef0746c9f109346f5432c1
swestlund <simonrtwestlund@gmail.com>
2025-09-04 14:39:37 +0200

    add form data to simonpage aannnnd test med MUI

--- blogg-grupp4/src/components/Nav.jsx
+++ blogg-grupp4/src/components/Nav.jsx
@@ -1,16 +1,18 @@
 import { HOME_PAGE, ESTELLE_PAGE, SIMON_PAGE, ANTON_PAGE } from "../App"
 
+
+
 export function Navbar({ changePage }) {
-return (
-      
-        <nav>
-          <ul>
-            <button onClick={() => changePage(HOME_PAGE)}>Home</button>
-            <button onClick={() => changePage(ANTON_PAGE)}>Anton</button>
-            <button onClick={() => changePage(ESTELLE_PAGE)}>Estelle</button>
-            <button onClick={() => changePage(SIMON_PAGE)}>Simon</button>
-          </ul>
-          </nav> 
-      
-);
+  return (
+
+    <nav>
+      <ul>
+        <button onClick={() => changePage(HOME_PAGE)}>Home</button>
+        <button onClick={() => changePage(ANTON_PAGE)}>Anton</button>
+        <button onClick={() => changePage(ESTELLE_PAGE)}>Estelle</button>
+        <button onClick={() => changePage(SIMON_PAGE)}>Simon</button>
+      </ul>
+    </nav>
+
+  );
 }
