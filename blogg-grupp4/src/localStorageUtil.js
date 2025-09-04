55385df1d59a59484fc49762ab43ae2d6ee668d8
anton <jappinen.anton@protonmail.com>
2025-09-04 17:05:56 +0200

    saving posts to localstorage

---
+++ blogg-grupp4/src/localStorageUtil.js
@@ -0,0 +1,13 @@
+export function loadPosts() {
+	const rawData = localStorage.getItem("posts")
+	return rawData ? JSON.parse(rawData) : []
+}
+
+export function savePosts(posts) {
+	localStorage.setItem("posts", JSON.stringify(posts))
+}
+
+export function removePost(id) {
+	const posts = loadPosts().filter((i) => i.id != id)
+	savePosts()
+}
