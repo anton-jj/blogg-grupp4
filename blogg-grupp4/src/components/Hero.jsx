47bc8026655995300a010dfa6d0d39ef01972366
swestlund <simonrtwestlund@gmail.com>
2025-09-18 14:50:27 +0200

    small changes and formatting

--- blogg-grupp4/src/components/Hero.jsx
+++ blogg-grupp4/src/components/Hero.jsx
@@ -1,17 +1,11 @@
 import "../styles/hero.css";
 export function Hero() {
-	const handleClick = () => {
-		alert("hello world");
-	};
 
 	return (
 		<>
 			<section className="hero">
 				<div className="hero-content">
 					<h1>Welcome to the blooogogogogogo</h1>
-					<button className="hero-button" onClick={handleClick}>
-						Explore bloggs
-					</button>
 				</div>
 			</section>
 		</>
