a8d36b4164f6316820024a32fdc765c7a8a9bca3
anton <jappinen.anton@protonmail.com>
2025-09-18 10:37:31 +0200

    button styling

--- blogg-grupp4/src/pages/anton.jsx
+++ blogg-grupp4/src/pages/anton.jsx
@@ -1,23 +1,30 @@
-import '../components/BlogCard'
-import { BlogCard } from '../components/BlogCard';
-import { Posts } from '../components/Posts'
-import "../anton.css"
+import "../components/BlogCard";
+import { BlogCard } from "../components/BlogCard";
+import { Posts } from "../components/Posts";
+import "../anton.css";
 
 export function AntonSida({ changePage, posts }) {
-
-	const antons = posts.filter((p) =>
-		p.author && p.author.toLowerCase() == "anton"
-	);
-	return (
-		<>
-			<h2 className='anton-heading'>antons’s Posts</h2>
-			{antons.length === 0 ? <p>No posts yet.</p> : (
-				<div className='anton-content-container'>
-					{antons.map((p) => (
-						<BlogCard key={p.id} title={p.title} author={p.author} content={p.content} date={p.createdAt} />
-					))}
-				</div>
-			)}
-		</>
-	);
+  const antons = posts.filter(
+    (p) => p.author && p.author.toLowerCase() == "anton"
+  );
+  return (
+    <>
+      <h2 className="anton-heading">antons’s Posts</h2>
+      {antons.length === 0 ? (
+        <p>No posts yet.</p>
+      ) : (
+        <div className="anton-content-container">
+          {antons.map((p) => (
+            <BlogCard
+              key={p.id}
+              title={p.title}
+              author={p.author}
+              content={p.content}
+              date={p.createdAt}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
